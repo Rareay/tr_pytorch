1d9208d2339c8d1879a8758c727c9a4a4d369191
Tamray <985480276@qq.com>
2021-02-21 20:23:05 +0800

    add expandImage.py

--- 8_expandImage.py
+++ 8_expandImage.py
@@ -1,7 +1,303 @@
+import cv2
+import numpy as np
+import random
+import os
 
 
 
-def doMove():
-    pass
+def findFile(path):
+    '''
+    return namelist
+    '''
+    namelist = []
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            namelist.append(os.path.join(root, file))
+    return namelist
 
-def 
+
+# 平移
+def doTranslation(img, w_rate, h_rate):
+    '''
+    :param w_off: -0.15  -  +0.15
+    :param h_off: -0.15  -  +0.15
+    '''
+    (h, w) = img.shape[:2]
+    w_off = int(w_rate * w)
+    h_off = int(h_rate * h)
+    M = np.float32([[1, 0, w_off], [0, 1, h_off]])
+    dst = cv2.warpAffine(img, M, (w, h))
+    return dst
+
+# 缩放
+def doResize(img, w_rate, h_rate, deviation):
+    '''
+    have some problem!
+    :param w_rate:
+    :param h_rate:
+    '''
+    (h, w) = img.shape[:2]
+    w_dst = int((w_rate + 1) * w)
+    h_dst = int((h_rate + 1) * h)
+    final_matrix = np.zeros(img.shape, np.uint8)
+    x1 = 0
+    y1 = h
+    x2 = w
+    y2 = 0
+    dst = cv2.resize(img, (w_dst, h_dst))
+    if h < h_dst:
+        final_matrix[y2:y1, x1:x2] = dst[y2+deviation:y1+deviation, x1+deviation:x2+deviation]
+    else:
+        if deviation == 0:
+            final_matrix[y2:h_dst, x1:w_dst] = dst[y2:h_dst, x1:w_dst:]
+        else:
+            final_matrix[y2+deviation:h_dst+deviation, x1+deviation:w_dst+deviation] = dst[y2+h_dst, x1:w_dst]
+    return final_matrix
+
+
+# 翻转
+def doFlip(img, direction):
+    '''
+    :param direction: 0 / 1  / -1
+    '''
+    out = img.copy()
+    cv2.flip(img, direction, out)
+    return  out
+
+
+
+# 旋转
+def doRotation(img, ra):
+    '''
+    :param ra: 0 - 360
+    '''
+    (h, w) = img.shape[:2]
+    center = (w/2, h/2)
+    M = cv2.getRotationMatrix2D(center, ra, 1.0)
+    dst = cv2.warpAffine(img, M, (w, h))
+    return dst
+ 
+# 剪切
+def doCut(img, top_rate=0., botton_rate=0., left_rate=0., right_rate=0.):
+    '''
+    :param top_rate:     0 - 0.2
+    :param bottom_rate:  0 - 0.2
+    :param left_rate:    0 - 0.2
+    :param right_rate:   0 - 0.2
+    '''
+    (h, w) = img.shape[:2]
+    top_off = int(h * top_rate)
+    botton_off = int(h * botton_rate)
+    left_off = int(w * left_rate)
+    right_off = int(w * right_rate)
+    x1 = 0
+    y1 = h
+    x2 = w
+    y2 = 0
+    final_matrix = np.zeros(img.shape, np.uint8)
+    final_matrix[y2 + top_off: y1 - botton_off, x1 + left_off:x2-right_off] = img[y2 + top_off:y1 - botton_off, x1 + left_off:x2 - right_off]
+
+    return final_matrix
+
+
+
+# 噪声
+def doNoiseJiaoyan(img, prob):
+    '''
+    :param prob: 0.001  -  0.01
+    '''
+    output = np.zeros(img.shape, np.uint8)
+    thres = 1 - prob
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            rdn = random.random()
+            if rdn < prob:
+                output[i][j] = 0
+            elif rdn > thres:
+                output[i][j] = 255
+            else:
+                output[i][j] = img[i][j]
+    return output
+
+
+def doNoiseGasuss(img, mean=0.5, var=0.02):
+    '''
+    :param mean: 0.1  -  0.5
+    :param var:  mean/20  -  mean/10
+    '''
+    img = img / 255
+    noise = np.random.normal(mean, var ** 0.5, img.shape)
+    out = img + noise
+    if out.min() < 0:
+        low_clip = -1.
+    else:
+        low_clip = 0.
+    out = np.clip(out, low_clip, 1.0)
+    out = np.uint8(out * 255)
+    return out
+
+# 模糊
+def doGasussBlur(img, sigma):
+    '''
+    :param kernel: (5,5)
+    :param sigma:  1.  -  3.
+    '''
+    out = cv2.GaussianBlur(img, (5,5), sigma)
+    return out
+
+
+def batchTranslation(namelist, save_dir, expand_num=0):
+    base_dir = save_dir + "/translation/"
+    if not os.path.exists(base_dir):
+        os.makedirs(base_dir)
+    filename_num = 0
+    obj_num = expand_num
+    for _ in range(obj_num):
+        index_rand = int(random.random() * (len(namelist) - 1))
+        filename = namelist[index_rand]
+        img = cv2.imread(filename)
+        w_rate = (random.random() - 0.5) * 1.5 / 5
+        h_rate = (random.random() - 0.5) * 1.5 / 5
+        img2 = doTranslation(img, w_rate, h_rate)
+        just_name = os.path.splitext(os.path.basename(filename))[0]
+        new_filename = base_dir + just_name + "_" + format(filename_num, '04d') + ".jpg"
+        cv2.imwrite(new_filename, img2)
+        filename_num += 1
+
+
+def batchFlip(namelist, save_dir, expand_num=0):
+    base_dir = save_dir + "/flip/"
+    if not os.path.exists(base_dir):
+        os.makedirs(base_dir)
+    filename_num = 0
+    obj_num = expand_num
+    for _ in range(obj_num):
+        index_rand = int(random.random() * (len(namelist) - 1))
+        filename = namelist[index_rand]
+        img = cv2.imread(filename)
+        direction = int(random.random() * 3) - 1
+        img2 = doFlip(img, direction)
+        just_name = os.path.splitext(os.path.basename(filename))[0]
+        new_filename = base_dir + just_name + "_" + format(filename_num, '04d') + ".jpg"
+        cv2.imwrite(new_filename, img2)
+        filename_num += 1
+
+
+def batchRotation(namelist, save_dir, expand_num=0):
+    base_dir = save_dir + "/rotation/"
+    if not os.path.exists(base_dir):
+        os.makedirs(base_dir)
+    filename_num = 0
+    obj_num = expand_num
+    for _ in range(obj_num):
+        index_rand = int(random.random() * (len(namelist) - 1))
+        filename = namelist[index_rand]
+        img = cv2.imread(filename)
+        rota = random.random() * 360
+        img2 = doRotation(img, rota)
+        just_name = os.path.splitext(os.path.basename(filename))[0]
+        new_filename = base_dir + just_name + "_" + format(filename_num, '04d') + ".jpg"
+        cv2.imwrite(new_filename, img2)
+        filename_num += 1
+
+def batchCut(namelist, save_dir, expand_num=0):
+    base_dir = save_dir + "/cut/"
+    if not os.path.exists(base_dir):
+        os.makedirs(base_dir)
+    filename_num = 0
+    obj_num = expand_num
+    for _ in range(obj_num):
+        index_rand = int(random.random() * (len(namelist) - 1))
+        filename = namelist[index_rand]
+        img = cv2.imread(filename)
+        rate = random.random() * 0.2
+        img2 = doCut(img, rate, rate, rate, rate)
+        just_name = os.path.splitext(os.path.basename(filename))[0]
+        new_filename = base_dir + just_name + "_" + format(filename_num, '04d') + ".jpg"
+        cv2.imwrite(new_filename, img2)
+        filename_num += 1
+
+
+
+def batchNoiseJiaoyan(namelist, save_dir, expand_num=0):
+    base_dir = save_dir + "/noise_jiaoyan/"
+    if not os.path.exists(base_dir):
+        os.makedirs(base_dir)
+    filename_num = 0
+    obj_num = expand_num
+    for _ in range(obj_num):
+        index_rand = int(random.random() * (len(namelist) - 1))
+        filename = namelist[index_rand]
+        img = cv2.imread(filename)
+        rate = 0.001 + 0.099 * random.random()
+        img2 = doNoiseJiaoyan(img, rate)
+        just_name = os.path.splitext(os.path.basename(filename))[0]
+        new_filename = base_dir + just_name + "_" + format(filename_num, '04d') + ".jpg"
+        cv2.imwrite(new_filename, img2)
+        filename_num += 1
+
+
+def batchNoiseGasuss(namelist, save_dir, expand_num=0):
+    base_dir = save_dir + "/noise_gasuss/"
+    if not os.path.exists(base_dir):
+        os.makedirs(base_dir)
+    filename_num = 0
+    obj_num = expand_num
+    for _ in range(obj_num):
+        index_rand = int(random.random() * (len(namelist) - 1))
+        filename = namelist[index_rand]
+        img = cv2.imread(filename)
+        mean = 0.1 + 0.4 * random.random()
+        var = mean / (10 + 10 * random.random())
+        img2 = doNoiseGasuss(img, mean, var)
+        just_name = os.path.splitext(os.path.basename(filename))[0]
+        new_filename = base_dir + just_name + "_" + format(filename_num, '04d') + ".jpg"
+        cv2.imwrite(new_filename, img2)
+        filename_num += 1
+
+def batchGasussBlur(namelist, save_dir, expand_num=0):
+    base_dir = save_dir + "/gasuss/"
+    if not os.path.exists(base_dir):
+        os.makedirs(base_dir)
+    filename_num = 0
+    obj_num = expand_num
+    for _ in range(obj_num):
+        index_rand = int(random.random() * (len(namelist) - 1))
+        filename = namelist[index_rand]
+        img = cv2.imread(filename)
+        sigma = 1 + 2 * random.random()
+        img2 = doGasussBlur(img, sigma)
+        just_name = os.path.splitext(os.path.basename(filename))[0]
+        new_filename = base_dir + just_name + "_" + format(filename_num, '04d') + ".jpg"
+        cv2.imwrite(new_filename, img2)
+        filename_num += 1
+
+
+
+def expandImage(image_dir, category, save_dir='./expandData',
+                translate=0,
+                flip=0,
+                rotation=0,
+                cut=0,
+                noise_jiaoyan=0,
+                noise_gasuss=0,
+                gasussblur=0):
+    save_dir = save_dir + "/" + category
+    namelist = findFile(image_dir)
+    batchTranslation(namelist, save_dir, translate)
+    batchFlip(namelist, save_dir, flip)
+    batchRotation(namelist, save_dir, rotation)
+    batchCut(namelist, save_dir, cut)
+    batchNoiseJiaoyan(namelist, save_dir, noise_jiaoyan)
+    batchNoiseGasuss(namelist, save_dir, noise_gasuss)
+    batchGasussBlur(namelist, save_dir, gasussblur)
+
+    print("Expand %d images, save in '%s'" %(translate + flip, save_dir))
+
+
+
+if __name__ == "__main__":
+    expandImage('data/test/gou', 'gou', translate=10, flip=10, rotation=10, cut=10,
+                                        noise_jiaoyan=10, noise_gasuss=10, gasussblur=10)
+
+
